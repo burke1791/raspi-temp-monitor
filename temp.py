852a1a79ec1c92c002d0b3ee61a262cc4a2f7bde
Chris Burke <burke1791@gmail.com>
2020-01-23 02:25:06 -0600

    initial commit

---
+++ temp.py
@@ -0,0 +1,42 @@
+import adafruit_dht
+import board
+from datetime import datetime
+import time
+
+# ----------- User Settings --------------
+
+# ----------------------------------------
+
+
+class bcolors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+
+
+dhtDevice = adafruit_dht.DHT22(board.D4)
+
+def monitor():
+	while True:
+		get_temp()
+		time.sleep(2.0)
+
+def get_temp():
+	try:
+		temp_c = dhtDevice.temperature
+		humidity = dhtDevice.humidity
+
+		t = datetime.now()
+		current_time = t.strftime('%m/%d/%Y %H:%M:%S')
+
+		print(f'{bcolors.BOLD}Time:{bcolors.ENDC} {bcolors.OKBLUE}{current_time}{bcolors.ENDC}   {bcolors.BOLD}Temp:{bcolors.ENDC} {bcolors.OKBLUE}{temp_c:.1f} C{bcolors.ENDC}    {bcolors.BOLD}Humidity:{bcolors.ENDC} {bcolors.OKBLUE}{humidity}%{bcolors.ENDC}')
+
+	except RuntimeError as error:
+		#print(error.args[0])
+		dummy = 5
+
+monitor()
